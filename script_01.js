3c127aa11746872136baaeb869aaf8bae7c86e99
Peter <schmidt.multimedia@gmail.com>
2022-07-12 11:57:23 +0200

    module: multiplication a * b

--- script_01.js
+++ script_01.js
@@ -9,10 +9,20 @@
 */
 
 
+// module: multiplication a * b |  test:
+output(multiply(2,3));
+output(multiply(2,-3));
+output(multiply(2,0));
+function multiply(a,b) {
+    return a * b;
+}
+
+
+
 // module: subtraction a - b |  test:
-output(subtract(2,3));
-output(subtract(2,-3));
-output(subtract(2,0));
+// output(subtract(2,3));
+// output(subtract(2,-3));
+// output(subtract(2,0));
 function subtract(a,b) {
     return a - b;
 }
